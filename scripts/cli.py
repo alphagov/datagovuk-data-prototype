dec54f61491805493db54fd9da29229a7dc515bd
Adam Shimali <adam.shimali@dsit.gov.uk>
2026-01-28 11:54:17 +0000

    task: add uk house price script

--- scripts/cli.py
+++ scripts/cli.py
@@ -2,6 +2,7 @@ import click
 from scripts.company_formation import companies_formed_by_quarter
 from scripts.csv_to_json import to_json
 from scripts.aggregate_fhrs_ratings_by_region import aggregate
+from scripts.uk_house_prices import get_prices
 
 
 @click.group
@@ -11,7 +12,8 @@ def cli():
 
 cli.add_command(companies_formed_by_quarter, name="companies")
 cli.add_command(to_json, name="tojson")
-cli.add_command(aggregate, name="ratings")
+cli.add_command(aggregate, name="food-ratings")
+cli.add_command(get_prices, name="house-prices")
 
 
 if __name__ == "__main__":
