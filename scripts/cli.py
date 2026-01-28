b5c68e713cd81cb45a9da6389ef51ed7f94757be
Adam Shimali <adam.shimali@dsit.gov.uk>
2026-01-28 10:48:55 +0000

    task: add food ratings. re-org reference data

--- scripts/cli.py
+++ scripts/cli.py
@@ -1,7 +1,7 @@
 import click
 from scripts.company_formation import companies_formed_by_quarter
 from scripts.csv_to_json import to_json
-from scripts.food_standards import add_region_to_ratings
+from scripts.aggregate_fhrs_ratings_by_region import aggregate
 
 
 @click.group
@@ -11,7 +11,7 @@ def cli():
 
 cli.add_command(companies_formed_by_quarter, name="companies")
 cli.add_command(to_json, name="tojson")
-cli.add_command(add_region_to_ratings, name="add-regions")
+cli.add_command(aggregate, name="ratings")
 
 
 if __name__ == "__main__":
